310577dd6489feddcf87ee5993ee4a4ce56c83c9
Roamin <roamin@tuta.io>
2022-12-25 16:27:33 -0500

    move to AS v0.2x.x

--- packages/as-proto/as-pect.config.js
+++ packages/as-proto/as-pect.config.js
@@ -1,30 +1,21 @@
-module.exports = {
+export default {
   /**
    * A set of globs passed to the glob package that qualify typescript files for testing.
    */
-  include: ["tests/**/*.spec.ts"],
+  entries: ["tests/**/*.spec.ts"],
   /**
    * A set of globs passed to the glob package that quality files to be added to each test.
    */
-  add: ["tests/**/*.include.ts"],
-  /**
-   * All the compiler flags needed for this test suite. Make sure that a binary file is output.
-   */
-  flags: {
-    /** To output a wat file, uncomment the following line. */
-    // "--textFile": ["output.wat"],
-    /** A runtime must be provided here. */
-    "--runtime": ["incremental"], // Acceptable values are: "incremental", "minimal", and "stub"
-  },
+  include: ["tests/**/*.include.ts"],
   /**
    * A set of regexp that will disclude source files from testing.
    */
-  disclude: [/node_modules/],
+  disclude: [/node_modules/i],
   /**
    * Add your required AssemblyScript imports here.
    */
-  imports(memory, createImports, instantiateSync, binary) {
-    return instantiateSync(binary, createImports({}));
+  async instantiate(memory, createImports, instantiate, binary) {
+    return instantiate(binary, createImports({}));
   },
   /** Enable code coverage. */
   // coverage: ["assembly/**/*.ts"],
